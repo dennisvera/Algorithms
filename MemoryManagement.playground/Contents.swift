2352c2e16dd4191c5e9d200f7b5701964f1947ec
dennisvera <dennisvera.z@gmail.com>
2017-04-12 19:23:58 -0400

    Memory management

---
+++ MemoryManagement.playground/Contents.swift
@@ -0,0 +1,44 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+class User {
+    var name: String
+    private(set) var phones: [Phone] = []
+    
+    init(name: String) {
+        self.name = name
+        print("User: \(name) is initialized")
+    }
+    
+    deinit {
+        print("User: \(name) is being deallocated")
+    }
+    
+    func add(phone: Phone) {
+        phones.append(phone)
+        phone.owner = self
+    }
+}
+
+class Phone {
+    let model: String
+    weak var owner: User?
+    
+    init(model: String) {
+        self.model = model
+        print("Phone: \(model) is initialized")
+    }
+    
+    deinit {
+        print("Phone: \(model) is being deallocated")
+    }
+}
+
+
+do {
+    let user1 = User(name: "John")
+    let iPhone = Phone(model: "iPhone 6s Plus")
+    user1.add(phone: iPhone)
+}
+
