f970cb16e01e11dfaeba344f1cf60bb0f59ec4f9
DennisVera <dennisvera.z@gmail.com>
2020-08-05 13:42:54 -0400

    Factorial and Recursive-Factorial Algorithm

--- Factorials&Recursion.playground/Contents.swift
+++ Factorials&Recursion.playground/Contents.swift
@@ -1,34 +1,34 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
 
-// Factorial algorithm
+// Factorial Algorithm
 
-func factorialOf(value: Int) -> Int {
-    
-    if value == 0 {
-        return 1
-    }
-    
-    var product: Int = 1
-    for i in 1...value {
-        product = product * i
-    }
-    
-    return product
+// A factorial solution
+func factorialOf(value: UInt) -> UInt {
+  var product: UInt = 1
+  
+  // The factorial value of 0 is 1
+  if value == 0 {
+    return 1
+  }
+  
+  for i in 1...value {
+    product = product * i
+  }
+  
+  return product
+}
+//print(factorialOf(value: 5))
+
+
+// A reccursive factorial solution
+func recursiveFactorialof(value: UInt) -> UInt {
+  // The factorial value of 0 is 1
+  if value == 0 {
+    return 1
+  }
+  
+  print(value)
+  return value * recursiveFactorialof(value: value - 1)
 }
 
-// Recursive-Factorial algorithm
-
-func recursiveFactorialOf(value: Int) -> Int {
-    
-    if value == 0 {
-        return 1
-    }
-    
-    return value * recursiveFactorialOf(value: value - 1)
-}
-
-print(recursiveFactorialOf(value: 5))
-print(factorialOf(value: 5))
-
+print(recursiveFactorialof(value: 3))
