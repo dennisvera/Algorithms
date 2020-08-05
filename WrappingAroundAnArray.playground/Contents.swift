0e4ffa6ee356f876ea13803a0e8ab5ef034d29ca
DennisVera <dennisvera.z@gmail.com>
2020-08-05 18:51:32 -0400

    Wrapp around algorithm

--- WrappingAroundAnArray.playground/Contents.swift
+++ WrappingAroundAnArray.playground/Contents.swift
@@ -1,36 +1,30 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
 
-// Wrapp around array alogorithm
+/// `Algorithm:` - Wrapp around array
 
+/// Assume we have a list of tracks
+let tracks = ["a", "b", "c", "d", "e", "f", "g"]
 
-// Assume we have a list of tracks
-let tracks = ["a", "b", "c", "d", "e"]
-
-// Algorithm should return this playlist if we select track "d"
+/// Algorithm should return this playlist if we select track "d"
 ["d", "e", "a", "b", "c"]
 
-var playlist = [String]()
 let selectedtrack = "d"
+var playlist = [String]()
 var priorTracks = [String]()
 
 for track in tracks {
-    if track == selectedtrack || playlist.count > 0 {
-        playlist.append(track)
-    } else {
-        priorTracks.append(track)
-    }
+  if track == selectedtrack || playlist.count > 0 {
+    playlist.append(track)
+  } else {
+    priorTracks.append(track)
+  }
 }
 
 print(playlist + priorTracks)
 
+/// Another solution
+let index = tracks.lastIndex(where: { return $0 == selectedtrack })
+let prefixArray = tracks.prefix(upTo: index ?? 0)
+let suffixArray = tracks.suffix(from: index ?? 0)
 
-// Solving algorithm with prefix & suffix
-
-let index = tracks.index(where: {return $0 == selectedtrack})
-let prefixArray = tracks.prefix(upTo: index!)
-let suffixArray = tracks.suffix(from: index!)
-let playlistArray = suffixArray + prefixArray
-
-print(playlistArray)
+print(suffixArray + prefixArray)
