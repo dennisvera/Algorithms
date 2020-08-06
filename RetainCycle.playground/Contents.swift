ee35afa5c3a18f2010166555dc244647e51fbfca
DennisVera <dennisvera.z@gmail.com>
2020-08-06 13:00:49 -0400

    Managing Retain cycles

--- RetainCycle.playground/Contents.swift
+++ RetainCycle.playground/Contents.swift
@@ -1,42 +1,36 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
 
-
-
-// Retain Cycle (memory management)
-
+/// `Retain Cycle` - Memory Management
 
 class Person {
-    let name: String
-    var apartment: Apartment?
-    init(name: String) {
-        self.name = name
-    }
-    
-    deinit {
-        print("\(name) is being deinitialized")
-    }
+  
+  let name: String
+  
+  init(name: String) {
+    self.name = name
+  }
+  
+  deinit {
+    print("\(name) is being deinitialized.")
+  }
 }
 
 class Apartment {
-    let number: Int
-    weak var tenant: Person?
-    
-    init(number: Int) {
-        self.number = number
-    }
-    
-    deinit {
-        print("Apartment \(number) is being deinitialized")
-    }
+  
+  let number: Int
+  /// Setting the tenant property to `weak` protects against retain cycles. Weak will not raise the `reference count`
+  weak var tenant: Person?
+  
+  init(number: Int) {
+    self.number = number
+  }
 }
 
 var bob: Person? = Person(name: "Bob")
-var apt: Apartment? = Apartment(number: 123)
-apt?.tenant = bob
-bob?.apartment = apt
+let apartment = Apartment(number: 666)
+
+/// Setting the tenant to Bob  without tenant  being `weak` property would create a  retain cycle.
+/// The solution to make sure you do not create a reatin cycle is to set one of the variables to weak.
+apartment.tenant = bob
 
 bob = nil
-apt = nil
-
