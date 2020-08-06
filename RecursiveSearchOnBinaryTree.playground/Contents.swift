ecd800d551ff8285b1317d6b6ff582a45b9af523
DennisVera <dennisvera.z@gmail.com>
2020-08-05 23:25:14 -0400

    Binary tree recursive search

--- RecursiveSearchOnBinaryTree.playground/Contents.swift
+++ RecursiveSearchOnBinaryTree.playground/Contents.swift
@@ -1,7 +1,6 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
 
+/// `Algorithm:` - Recursive Search Binary Tree
 
 // 1. Binary Tree
 //
@@ -14,57 +13,55 @@ import UIKit
 
 // 2.
 class Node {
-    let value: Int
-    let leftChild: Node?
-    let rightChild: Node?
-    
-    init(value: Int, leftChild: Node?, rightChild: Node?) {
-        self.value = value
-        self.leftChild = leftChild
-        self.rightChild = rightChild
-    }
+  let value: Int
+  var ledftChild: Node?
+  var rightChild: Node?
+  
+  init(value: Int, ledftChild: Node?, rightChild: Node?) {
+    self.value = value
+    self.ledftChild = ledftChild
+    self.rightChild = rightChild
+  }
 }
 
-//left branch
-let oneNode = Node(value: 1, leftChild: nil, rightChild: nil)
-let fiveNode = Node(value: 5, leftChild: oneNode, rightChild: nil)
+/// Left branch nodes
+let oneNode = Node(value: 1, ledftChild: nil, rightChild: nil)
+let fiveNode = Node(value: 5, ledftChild: oneNode, rightChild: nil)
 
-//right branch
-let elevenNode = Node(value: 11, leftChild: nil, rightChild: nil)
-let twentyNode = Node(value: 20, leftChild: nil, rightChild: nil)
-let fourteenNode = Node(value: 14, leftChild: elevenNode, rightChild: twentyNode)
+/// Right branch nodes
+let elevenNode = Node(value: 11, ledftChild: nil, rightChild: nil)
+let twentyNode = Node(value: 20, ledftChild: nil, rightChild: nil)
+let fourTeenNode = Node(value: 14, ledftChild: elevenNode, rightChild: twentyNode)
 
-//root node
-let tenRootNode = Node(value: 10, leftChild: fiveNode, rightChild: fourteenNode)
-
+/// Root nodes
+let tenRootNode = Node(value: 10, ledftChild: fiveNode, rightChild: fourTeenNode)
 
 // 3.
-//Interviewer's question:Implement a search algorithm that searches through this tree for a particular searchValue
+// Interview question:
+// Implement a search algorithm that searches through this tree for a particular searchValue
+
 func search(node: Node?, searchValue: Int) -> Bool {
-    if node == nil {
-        return false
-    }
-    
-    if node?.value == searchValue {
-        return true
-    } else {
-        //recursion
-        return search(node: node?.leftChild, searchValue: searchValue) || search(node: node?.rightChild, searchValue: searchValue)
-    }
+  if node == nil {
+    return false
+  }
+  
+  if node?.value == searchValue {
+    return true
+  } else if searchValue < node!.value {
+    // recursion
+    return search(node: node?.ledftChild, searchValue: searchValue)
+  } else {
+    // recursion
+    return search(node: node?.rightChild, searchValue: searchValue)
+  }
 }
 
 search(node: tenRootNode, searchValue: 20)
 
-
-
 // 4.
-//index search
+// What is the point of binary search?
+// A: Efficiency: the binary is much faster than a for-lop or index search.
 
+// Index search
 let list = [1, 5, 10, 11, 14, 20]
-let index = list.index(where: {$0 == 5})
-index
-
-
-
-
-
+let index = list.firstIndex(where: { $0 == 20 })
